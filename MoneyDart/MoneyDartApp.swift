1ab7a258239ca54ddd8b50749fab2185c183b970
rangerboy574 <jdeweese574@gmail.com>
2022-12-18 17:40:25 -0600

    Initial Commit

---
+++ MoneyDart/MoneyDartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyDartApp.swift
+//  MoneyDart
+//
+//  Created by Joseph  DeWeese on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MoneyDartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
